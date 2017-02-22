48747c422a471a594578e07c04a4b8490fe7341d
Chaosman004 <mr.robert.page@gmail.com>
2017-02-22 10:14:21 -0500

    done what i can

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,30 @@
+/**
+ * File name: home.ts
+ * Author: Robert Page
+ * Student	ID: 200281838
+ * Date: 2017-02-22
+ */
+
+// Imports
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+// Component
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+// constructor
   constructor(public navCtrl: NavController) {
 
   }
 
+// Methods
+
+// cancel method
+  cancel(){
+    
+  }
 }
